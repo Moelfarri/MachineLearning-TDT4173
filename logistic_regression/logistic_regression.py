8d0778ac318168bc9b87f9606e65693175cf092c
Elias Elfarri <47497819+Moelfarri@users.noreply.github.com>
2021-08-26 18:29:02 +0200

    Add files via upload

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -6,10 +6,15 @@ import pandas as pd
 
 class LogisticRegression:
     
-    def __init__():
-        # NOTE: Feel free add any hyperparameters 
-        # (with defaults) as you see fit
-        pass
+    def __init__(self, lr=0.01,iterations=1000):
+        """Implementing Logistic Regression
+        with SGD as an optimizer - skleran uses SAG.
+        """
+        self.lr         = lr
+        self.iterations = iterations
+        self.weights    = None
+        self.bias       = 0
+        
         
     def fit(self, X, y):
         """
@@ -21,8 +26,27 @@ class LogisticRegression:
             y (array<m>): a vector of floats containing 
                 m binary 0.0/1.0 labels
         """
-        # TODO: Implement
-        raise NotImplemented()
+        m_samples, n_features = np.shape(X)
+        self.weights          = np.zeros(n_features) #init weights as zeros
+      
+        
+        
+        for i in range(self.iterations):
+            #forward
+            f = X@self.weights + np.array(self.bias) #w*x + b
+            y_pred = sigmoid(f)
+              
+            assert y.shape == y_pred.shape
+            
+            #minimize cost function with respect to weights and bias
+            dw = -(1/n_features)*X.T@(y-y_pred) 
+            db = -(1/n_features)*np.sum(y-y_pred)
+            
+            #Stochastic Gradient Descent (backstepping)
+            self.weights = gradient_step(self.weights, dw, self.lr)
+            self.bias    = gradient_step(self.bias, db, self.lr)
+
+
     
     def predict(self, X):
         """
@@ -38,8 +62,10 @@ class LogisticRegression:
             A length m array of floats in the range [0, 1]
             with probability-like predictions
         """
-        # TODO: Implement
-        raise NotImplemented()
+        f = X@self.weights + np.array(self.bias) 
+        y_pred = sigmoid(f)
+        return y_pred
+
         
 
         
@@ -97,4 +123,6 @@ def sigmoid(x):
     """
     return 1. / (1. + np.exp(-x))
 
-        
+
+def gradient_step(x, dx, lr):
+    return x - lr*dx
